ea8ceadc8ebe516c3c86ecabe93e99f31f7c4ab1
koki-develop <kou.pg.0131@gmail.com>
2022-09-11 20:20:23 +0900

    ヘッダを作成

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,7 @@
-import React, { memo } from "react";
+import Link from "next/link";
+import React, { memo, useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import Drawer from "../util/Drawer";
 
 export type LayoutProps = {
   children: React.ReactNode;
@@ -7,7 +10,31 @@ export type LayoutProps = {
 const Layout: React.FC<LayoutProps> = memo((props) => {
   const { children } = props;
 
-  return <div>{children}</div>;
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
+  const handleOpenMenu = useCallback(() => setOpenMenu((prev) => !prev), []);
+  const handleCloseMenu = useCallback(() => setOpenMenu(false), []);
+
+  return (
+    <div>
+      <header className="flex items-center bg-teal-500 px-6 py-4">
+        <div className="flex text-white font-semibold text-xl">
+          <button className="mr-2" onClick={handleOpenMenu}>
+            <AiOutlineMenu />
+          </button>
+          <h1>
+            <Link href="/">iToolkit</Link>
+          </h1>
+        </div>
+      </header>
+
+      <Drawer open={openMenu} onClose={handleCloseMenu}>
+        drawer content
+      </Drawer>
+
+      <main>{children}</main>
+    </div>
+  );
 });
 
 Layout.displayName = "Layout";
