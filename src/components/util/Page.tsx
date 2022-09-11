5d1ac48a84e5177051186b3a675414d55aab897a
koki-develop <kou.pg.0131@gmail.com>
2022-09-12 08:13:43 +0900

    Page コンポーネントを作成

---
+++ src/components/util/Page.tsx
@@ -0,0 +1,22 @@
+import React, { memo } from "react";
+import Layout from "../Layout";
+
+export type PageProps = {
+  children: React.ReactNode;
+  title: string;
+};
+
+const Page: React.FC<PageProps> = memo((props) => {
+  const { children, title } = props;
+
+  return (
+    <Layout>
+      <h2 className="text-2xl mb-2">{title}</h2>
+      {children}
+    </Layout>
+  );
+});
+
+Page.displayName = "Page";
+
+export default Page;
