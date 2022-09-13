6897c4cd499836684a447e96ac15e5efa466fab1
koki-develop <kou.pg.0131@gmail.com>
2022-09-14 08:22:53 +0900

    import/order rule の設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,21 @@ module.exports = {
   plugins: ["unused-imports"],
   rules: {
     "unused-imports/no-unused-imports": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "object",
+          "type",
+          "index",
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
   },
 };
