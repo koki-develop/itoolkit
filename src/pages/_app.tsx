ad017aa42fcb14b6ec07e901c089ad48e86b5a6b
koki-develop <kou.pg.0131@gmail.com>
2022-09-14 12:06:23 +0900

    nprogress を導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,32 @@
+import { useRouter } from "next/router";
+import nProgress from "nprogress";
+import React, { useCallback, useEffect } from "react";
 import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
-import "@/styles/global.css";
+import "@/styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const handleRouteStart = useCallback(() => {
+    nProgress.start();
+  }, []);
+
+  const handleRouteDone = useCallback(() => {
+    nProgress.done();
+  }, []);
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", handleRouteStart);
+    router.events.on("routeChangeComplete", handleRouteDone);
+    router.events.on("routeChangeError", handleRouteDone);
+    return () => {
+      router.events.off("routeChangeStart", handleRouteStart);
+      router.events.off("routeChangeComplete", handleRouteDone);
+      router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, [handleRouteDone, handleRouteStart, router.events]);
+
   return (
     <Layout>
       <Component {...pageProps} />
