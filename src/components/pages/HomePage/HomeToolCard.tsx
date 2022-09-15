6c99d258d48633432146a0362925af33a4631df2
koki-develop <kou.pg.0131@gmail.com>
2022-09-15 20:34:46 +0900

    HomeToolCard の見た目を修正

--- src/components/pages/HomePage/HomeToolCard.tsx
+++ src/components/pages/HomePage/HomeToolCard.tsx
@@ -11,8 +11,10 @@ const HomeToolCard: React.FC<HomeToolCardProps> = memo(props => {
 
   return (
     <Link key={tool.href} href={tool.href}>
-      <a className="flex flex-col items-center justify-center rounded border py-8 text-xs hover:bg-gray-100 active:bg-gray-200">
-        <div>{React.createElement(tool.icon, { className: "text-3xl" })}</div>
+      <a className="flex flex-col items-center justify-center rounded border py-8 text-xs hover:bg-gray-100 active:bg-gray-200 dark:border-stone-700 dark:bg-stone-800 dark:hover:bg-stone-700 dark:active:bg-stone-600 md:text-sm lg:text-base">
+        <div className="mb-2">
+          {React.createElement(tool.icon, { className: "text-3xl" })}
+        </div>
         <div>{tool.title}</div>
       </a>
     </Link>
