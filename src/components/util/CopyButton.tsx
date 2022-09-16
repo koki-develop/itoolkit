17e862e963a13f961e751bb10ce1ea8e328b79f4
koki-develop <kou.pg.0131@gmail.com>
2022-09-16 10:51:15 +0900

    button の outline はつけといた

--- src/components/util/CopyButton.tsx
+++ src/components/util/CopyButton.tsx
@@ -37,7 +37,7 @@ const CopyButton: React.FC<CopyButtonProps> = memo(props => {
       {...buttonProps}
       className={classNames(
         buttonProps.className,
-        "rounded border p-2 outline-none hover:bg-gray-100 active:bg-gray-200 dark:border-stone-700 dark:bg-stone-800 dark:hover:bg-stone-700 dark:active:bg-stone-600",
+        "rounded border p-2 hover:bg-gray-100 active:bg-gray-200 dark:border-stone-700 dark:bg-stone-800 dark:hover:bg-stone-700 dark:active:bg-stone-600",
       )}
       onClick={handleCopy}
     >
