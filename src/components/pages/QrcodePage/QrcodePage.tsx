293f0a48cdf1d9304a011211e88e9d996458a485
koki-develop <kou.pg.0131@gmail.com>
2022-09-16 13:30:59 +0900

    QR Code page を作成

--- src/components/pages/QrcodePage/QrcodePage.tsx
+++ src/components/pages/QrcodePage/QrcodePage.tsx
@@ -1,8 +1,58 @@
 import { NextPage } from "next";
-import React from "react";
+import Image from "next/image";
+import qrcode from "qrcode";
+import React, { useCallback, useState } from "react";
+import Page from "@/components/util/Page";
+import TextArea from "@/components/util/TextArea";
 
 const QrcodePage: NextPage = () => {
-  return <div>qrcode</div>;
+  const [text, setText] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [qrcodeSrc, setQrcodeSrc] = useState<string | null>(null);
+
+  const handleChangeText = useCallback((value: string) => {
+    setText(value);
+
+    if (value == "") {
+      setQrcodeSrc(null);
+      setError(null);
+    } else {
+      qrcode
+        .toDataURL(value)
+        .then(src => {
+          setQrcodeSrc(src);
+          setError(null);
+        })
+        .catch(err => setError(err.message));
+    }
+  }, []);
+
+  return (
+    <Page title="QR Code">
+      <TextArea
+        title="Text"
+        error={error}
+        value={text}
+        onChange={handleChangeText}
+        textareaProps={{
+          className: "mb-8",
+          rows: 5,
+        }}
+      />
+
+      {qrcodeSrc && (
+        <div className="flex justify-center">
+          <Image
+            className="max-w-full"
+            width={300}
+            height={300}
+            src={qrcodeSrc}
+            alt="QR Code"
+          />
+        </div>
+      )}
+    </Page>
+  );
 };
 
 export default QrcodePage;
