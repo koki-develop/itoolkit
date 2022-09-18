324f7d4a5fefaa17900ef0ace18016d19be098bc
koki-develop <kou.pg.0131@gmail.com>
2022-09-18 09:06:49 +0900

    NEXT_PUBLIC_VERCEL_ENV 環境変数を追加

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,9 @@ declare module "*.svg" {
   const content: React.FC<React.SVGProps<SVGElement>>;
   export default content;
 }
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NEXT_PUBLIC_VERCEL_ENV: "production" | "preview" | "development";
+  }
+}
