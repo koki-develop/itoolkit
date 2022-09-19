c727d11a2dcd0384aaf94cf05c507b557ec78884
koki-develop <kou.pg.0131@gmail.com>
2022-09-19 09:10:00 +0900

    home page でツールを検索できるようにした

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,15 +1,46 @@
 import { NextPage } from "next";
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 import Page from "@/components/util/Page";
 import { useToolGroups, useTools } from "@/hooks/toolHooks";
 import HomeToolCard from "./HomeToolCard";
 
 const HomePage: NextPage = () => {
-  const tools = useTools("");
+  const [searchText, setSearchText] = useState<string>("");
+
+  const tools = useTools(searchText);
   const groups = useToolGroups(tools);
+
+  const searchInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleClickSearchInput = useCallback(() => {
+    searchInputRef.current?.focus();
+  }, []);
+
+  const handleChangeSearchText = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchText(event.currentTarget.value);
+    },
+    [],
+  );
 
   return (
     <Page>
+      <div
+        className="my-4 flex w-full cursor-text items-center overflow-hidden rounded border py-1 pl-2 dark:border-stone-600"
+        onClick={handleClickSearchInput}
+      >
+        <AiOutlineSearch className="mr-1" />
+        <input
+          ref={searchInputRef}
+          placeholder="Search Tools (/)"
+          className="w-0 grow bg-transparent py-1 pr-2 outline-none"
+          type="text"
+          value={searchText}
+          onChange={handleChangeSearchText}
+        />
+      </div>
+
       {groups.map(group => (
         <div key={group.name} className="mb-4">
           <div className="mb-2">{group.name}</div>
