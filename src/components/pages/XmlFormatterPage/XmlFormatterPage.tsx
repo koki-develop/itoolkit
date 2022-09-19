ef2b0ef2d3cddca9976105881ff95f96dc3a61cc
koki-develop <kou.pg.0131@gmail.com>
2022-09-20 08:17:47 +0900

    xml formatter page を実装

--- src/components/pages/XmlFormatterPage/XmlFormatterPage.tsx
+++ src/components/pages/XmlFormatterPage/XmlFormatterPage.tsx
@@ -1,18 +1,22 @@
 import { NextPage } from "next";
 import React from "react";
-import Page from "@/components/util/Page";
+import FormatterPage from "@/components/util/FormatterPage";
 import { useI18n } from "@/hooks/i18nHooks";
+import { useFormat } from "@/hooks/libHooks";
 
 const XmlFormatterPage: NextPage = () => {
   const { t } = useI18n();
 
+  const { formatXml } = useFormat();
+
   return (
-    <Page
+    <FormatterPage
       title={t.tools.xmlFormatter.name}
       description={t.tools.xmlFormatter.description}
-    >
-      xml
-    </Page>
+      left={{ title: t.words.xml }}
+      right={{ title: t.words.formattedXml }}
+      format={formatXml}
+    />
   );
 };
 
