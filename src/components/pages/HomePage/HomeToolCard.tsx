ee2180d9b2edb30ba3c1a2659484cef875efba5a
koki-develop <kou.pg.0131@gmail.com>
2022-09-24 10:48:25 +0900

    HomeToolCard に Button を適用

--- src/components/pages/HomePage/HomeToolCard.tsx
+++ src/components/pages/HomePage/HomeToolCard.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import Link from "@/components/util/Link";
+import LinkButton from "@/components/util/LinkButton";
 import { Tool } from "@/types/tool";
 
 export type HomeToolCardProps = {
@@ -10,15 +10,15 @@ const HomeToolCard: React.FC<HomeToolCardProps> = memo(props => {
   const { tool } = props;
 
   return (
-    <Link
+    <LinkButton
       href={tool.href}
-      className="flex flex-col items-center justify-center rounded border py-8 text-xs hover:bg-gray-100 active:bg-gray-200 dark:border-stone-700 dark:bg-stone-800 dark:hover:bg-stone-700 dark:active:bg-stone-600 md:text-sm lg:text-base"
+      className="flex w-full flex-col rounded border py-8 text-xs dark:border-stone-700 dark:bg-stone-800 md:text-sm lg:text-base"
     >
       <div className="mb-2">
         {React.createElement(tool.icon, { className: "text-3xl" })}
       </div>
       <div className="px-2 text-center">{tool.name}</div>
-    </Link>
+    </LinkButton>
   );
 });
 
