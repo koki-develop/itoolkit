684b71c700d06dba633948cc10cab7111b28f2ea
koki-develop <kou.pg.0131@gmail.com>
2022-09-24 09:00:40 +0900

    Link コンポーネントを作成

---
+++ src/components/util/Link.tsx
@@ -0,0 +1,28 @@
+import NextLink from "next/link";
+import React from "react";
+
+type LinkProps = Omit<React.HTMLProps<HTMLAnchorElement>, "href"> & {
+  href: string;
+  locale?: string;
+  external?: boolean;
+};
+
+const Link: React.FC<LinkProps> = React.memo(props => {
+  const { external, locale, ...linkProps } = props;
+
+  if (external) {
+    return <a target="_blank" rel="noreferrer noopener" {...linkProps} />;
+  }
+
+  const { href, ...otherLinkProps } = linkProps;
+
+  return (
+    <NextLink href={href} locale={locale}>
+      <a {...otherLinkProps} />
+    </NextLink>
+  );
+});
+
+Link.displayName = "Link";
+
+export default Link;
