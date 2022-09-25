40128349ffef3c27221fa80071af9634b71fccd6
koki-develop <kou.pg.0131@gmail.com>
2022-09-25 09:38:41 +0900

    alternateRefs を設定

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,10 @@
 module.exports = {
   siteUrl: "https://itoolkit.dev",
   generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: "https://itoolkit.dev/ja",
+      hreflang: "ja",
+    },
+  ],
 };
