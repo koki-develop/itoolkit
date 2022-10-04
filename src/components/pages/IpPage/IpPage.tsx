112f5551639c182e416c439666cebd8e93975be4
koki-develop <kou.pg.0131@gmail.com>
2022-10-05 08:56:16 +0900

    IP アドレス表示するところまで

--- src/components/pages/IpPage/IpPage.tsx
+++ src/components/pages/IpPage/IpPage.tsx
@@ -1,13 +1,31 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 import Page from "@/components/util/Page";
 import { useI18n } from "@/hooks/i18nHooks";
+import { useIp } from "@/hooks/libHooks";
 
 const IpPage: NextPage = () => {
+  const [ip, setIp] = useState<string | null>(null);
+  const [fetchedIp, setFetchedIp] = useState<boolean>(false);
+
   const { t } = useI18n();
+  const { fetchIp } = useIp();
+
+  useEffect(() => {
+    fetchIp()
+      .then(ip => {
+        setIp(ip);
+      })
+      .catch(() => setIp(null))
+      .finally(() => {
+        setFetchedIp(true);
+      });
+  }, [fetchIp]);
 
   return (
     <Page title={t.tools.ip.name} description={t.tools.ip.description}>
-      ip
+      {!fetchedIp && <div>fetching...</div>}
+      {fetchedIp && (ip ? <div>{ip}</div> : <div>failed</div>)}
     </Page>
   );
 };
