6afba2e4d9f0519147920fe715eb54a7022f9a9b
koki-develop <kou.pg.0131@gmail.com>
2022-10-05 07:08:02 +0900

    footer の fontsize を sm -> xs

--- src/components/Layout/LayoutMenuFotter.tsx
+++ src/components/Layout/LayoutMenuFotter.tsx
@@ -6,7 +6,7 @@ const LayoutMenuFooter: React.FC = memo(() => {
   const { t } = useI18n();
 
   return (
-    <div className="-mt-[1px] flex flex-col items-center justify-center border-t py-2 text-sm dark:border-t-stone-600">
+    <div className="-mt-[1px] flex flex-col items-center justify-center border-t py-2 text-xs dark:border-t-stone-600">
       <div className="mb-1">&copy;2022</div>
       <div>
         <Link href="/privacy">{t.privacy.title}</Link>
