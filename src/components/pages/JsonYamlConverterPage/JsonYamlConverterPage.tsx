74c7c51ecfeaa8fc7c64ce553add690d3562df9f
koki-develop <kou.pg.0131@gmail.com>
2022-10-06 21:51:20 +0900

    とりあえず動くところまで実装

--- src/components/pages/JsonYamlConverterPage/JsonYamlConverterPage.tsx
+++ src/components/pages/JsonYamlConverterPage/JsonYamlConverterPage.tsx
@@ -1,17 +1,38 @@
+import YAML from "js-yaml";
 import { NextPage } from "next";
-import React from "react";
+import React, { useCallback } from "react";
 import Page from "@/components/util/Page";
+import TextAreas from "@/components/util/TextAreas";
 import { useI18n } from "@/hooks/i18nHooks";
 
 const JsonYamlConverterPage: NextPage = () => {
   const { t } = useI18n();
+
+  const jsonToYaml = useCallback((json: string): string => {
+    const parsed = JSON.parse(json);
+    return YAML.dump(parsed);
+  }, []);
+
+  const yamlToJson = useCallback((yaml: string): string => {
+    const parsed = YAML.load(yaml);
+    return JSON.stringify(parsed, null, 4);
+  }, []);
 
   return (
     <Page
       title={t.tools.jsonYamlConverter.name}
       description={t.tools.jsonYamlConverter.description}
     >
-      json yaml converter
+      <TextAreas
+        left={{
+          title: "JSON",
+          toRightFunc: jsonToYaml,
+        }}
+        right={{
+          title: "YAML",
+          toLeftFunc: yamlToJson,
+        }}
+      />
     </Page>
   );
 };
