8a9c72d8268fce1e12d141a8037a22953dc2522b
koki-develop <kou.pg.0131@gmail.com>
2022-10-07 21:16:31 +0900

    JsonYaml icon を作成

--- src/components/util/icons/index.tsx
+++ src/components/util/icons/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IconType } from "react-icons";
 import Base64Icon from "./base64.svg";
+import JsonYamlIcon from "./jsonYaml.svg";
 
 type IconProps = React.SVGProps<SVGElement>;
 type CustomIcon = React.FC<IconProps>;
@@ -9,4 +10,8 @@ export type Icon = CustomIcon | IconType;
 
 export const Base64: CustomIcon = props => {
   return <Base64Icon {...props} />;
+};
+
+export const JsonYaml: CustomIcon = props => {
+  return <JsonYamlIcon {...props} />;
 };
